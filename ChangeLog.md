92079e8c475594a9dc580bc24f193dfa22d46a7d
John BOTELLA <john.botella@atm-consulting.fr>
2024-04-22 12:10:31 +0200

    Release 2.0

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,4 +1,13 @@
-#2.0
+## Unreleased 
+
+
+
+## Release 2.0
 
 - FIX : Update Translations - 2024-04-11 - 2.0.1
-- NEW : Compatibility v19 - 2024-04-10 - 2.0
+- NEW : Compatibility v19 - 2024-04-10 - 2.0   
+  lose compatiblility under Dolibarr V12
+
+# Release 1.0
+
+- Initial version
--- core/modules/modDandelion.class.php
+++ core/modules/modDandelion.class.php
@@ -54,7 +54,7 @@ class modDandelion extends DolibarrModules
 
 		// Family can be 'crm','financial','hr','projects','products','ecm','technic','other'
 		// It is used to group modules in module setup page
-		$this->family = "ATM";
+		$this->family = "other";
 		// Module label (no space allowed), used if translation string 'ModuleXXXName' not found (where XXX is value of numeric property 'numero' of module)
 		$this->name = preg_replace('/^mod/i','',get_class($this));
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
@@ -68,7 +68,7 @@ class modDandelion extends DolibarrModules
 		// Name of image file used for this module.
 		// If file is in theme/yourtheme/img directory under name object_pictovalue.png, use this->picto='pictovalue'
 		// If file is in module/img directory under name object_pictovalue.png, use this->picto='pictovalue@module'
-		$this->picto='dandelion@dandelion';
+		$this->picto='logo.svg@dandelion';
 
 		// Defined all module parts (triggers, login, substitutions, menus, css, etc...)
 		// for default path (eg: /dandelion/core/xxxxx) (0=disable, 1=enable)
